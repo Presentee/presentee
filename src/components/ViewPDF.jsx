819a45ce24cdef13a7e2707aad669d654f005a8b
Christopher Liudahl-Rackley <53634121+clrminded@users.noreply.github.com>
2022-11-22 17:45:14 -0800

    added viewpdf.jsx

---
+++ src/components/ViewPDF.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import '../PDF_viewer.css'
+import {Viewer, Worker} from '@react-pdf-viewer/core'
+import {defaultLayoutPlugin} from '@react-pdf-viewer/default-layout'
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+
+export default function ViewPDF(params) {
+
+    const [viewPdf, setViewPdf] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // if pdf file is not null
+        if(params.pdfFile !== null){
+          setViewPdf(params.pdfFile)
+        }
+        else {
+          setViewPdf(null)
+        }
+      }
+
+      const newplugin= defaultLayoutPlugin()
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <button type='submit' className='btn btn-success'> View Presentee </button>
+            </form>
+
+            <div className='pdf-container'>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+                    {viewPdf && <>
+                        <Viewer fileUrl={viewPdf} plugins={[newplugin]} />
+                    </>}
+                    {!viewPdf && <>No PDF</>}
+                </Worker>
+            </div>
+        </div>
+
+    )
+};
