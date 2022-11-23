0f1f3d3422806089bbd4988f54716766f12f74a6
Christopher Liudahl-Rackley <53634121+clrminded@users.noreply.github.com>
2022-11-22 17:43:32 -0800

    added pdf.jsx

---
+++ src/components/Pdf.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import './PDF_viewer.css'
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import FileUpload from './components/FileUpload'
+import ViewPDF from './components/ViewPDF'
+
+function App(){
+
+  const [pdfFile,setPDFFile] = useState(null)
+
+  return (
+    <div className = 'container'>
+
+      {/* Fileupload Box */}
+      <FileUpload setPDFFile={setPDFFile} />
+
+      <h2> View Presentee </h2>
+
+      {/* View PDF Box */}
+      <ViewPDF pdfFile={pdfFile} />
+      
+    </div>
+  )
+}
+
+export default App
