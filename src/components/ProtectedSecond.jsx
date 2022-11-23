7aa87df7fe43802fa46612535d36125108f43e57
Christopher Liudahl-Rackley <53634121+clrminded@users.noreply.github.com>
2022-11-22 18:40:42 -0800

    working pdf viewer

--- src/components/ProtectedSecond.jsx
+++ src/components/ProtectedSecond.jsx
@@ -1,16 +1,19 @@
 // components/ProtectSecond.js
-import { useAuthenticator, Heading } from '@aws-amplify/ui-react';
+
 import Pdf from './Pdf'
 
 const ProtectedSecond = () => {
-  const { route } = useAuthenticator((context) => [context.route]);
 
   /*
   const message =
     route === 'authenticated' ? 'SECOND PROTECTED ROUTE!' : 'Loading...';
   return <Heading level={1}>{message}</Heading>;
   */
-  <Pdf />
+  return (
+    <div>
+      <Pdf />
+    </div>
+  );
 }
 
 export default ProtectedSecond;
