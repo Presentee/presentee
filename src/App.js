222f2943c286988d72873b2da944b8ee6e0805d6
clrminded <the_rocket_scientist@icloud.com>
2023-01-11 01:17:14 -0800

    moved PresenteeRoutes into its own component

--- src/App.js
+++ src/App.js
@@ -1,71 +1,9 @@
 // App.js
 import { Authenticator } from '@aws-amplify/ui-react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
+import PresenteeRoutes from './components/PresenteeRoutes';
 
 import "./App.css"
 
-import Protected from './components/Protected';
-import RequireAuth from './RequireAuth';
-import Login from './components/Login';
-import ProtectedSecond from './components/ProtectedSecond';
-import Home from './components/Home';
-import Layout from './components/Layout';
-import Create from './components/Create';
-
-/* The PresenteeRoutes function will provide all the routes 
-that are needed to navigate between urls when such buttons 
-are clicked. */
-const PresenteeRoutes = () => {
-
-  const [pdfFile, setPDFFile] = useState(null)
-  
-  return (
-    <BrowserRouter>
-      {/* Routes of App */}
-      <Routes>
-        {/* Parent compontent Layout
-        This holds the routes of all children routes. 
-        This means that inside of Layout there are components
-        that need to be routed to different urls and these
-        are provided here. */}
-        <Route path="/" element={<Layout />}>
-          {/* Child component Home*/}
-          <Route index element={<Home />} />
-          {/* Child component Protected*/}
-          <Route
-            path="/protected"
-            element={
-              <RequireAuth>
-                <Protected />
-              </RequireAuth>
-            }
-          />
-          {/* Child component ProtectedSecond */}
-          <Route
-            path="/protected2"
-            element={
-              <RequireAuth>
-                <ProtectedSecond pdfFile={pdfFile}/>
-              </RequireAuth>
-            }
-          />
-          <Route path="/create" element={
-          <RequireAuth>
-            <Create setPDFFile={setPDFFile} pdfFile={pdfFile}/>
-          </RequireAuth>
-          } />
-          {/* Child component Login */}
-          <Route path="/login" element={<Login />} />
-        {/* End of parent component Layout */}
-        </Route>
-
-        
-      </Routes>
-    </BrowserRouter>
-  )
-} /* End of Presentee Routes function */
- 
 const App = () => {
   return (
       <Authenticator.Provider>
---
+++ src/components/PresenteeRoutes.js
@@ -0,0 +1,66 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+
+import Protected from './Protected';
+import RequireAuth from './RequireAuth';
+import Login from './Login';
+import ProtectedSecond from './ProtectedSecond';
+import Home from './Home';
+import Layout from './Layout';
+import Create from './Create';
+
+/* The PresenteeRoutes function will provide all the routes 
+that are needed to navigate between urls when such buttons 
+are clicked. */
+
+const PresenteeRoutes = () => {
+
+    const [pdfFile, setPDFFile] = useState()
+    
+    return (
+      <BrowserRouter>
+        {/* Routes of App */}
+        <Routes>
+          {/* Parent compontent Layout
+          This holds the routes of all children routes. 
+          This means that inside of Layout there are components
+          that need to be routed to different urls and these
+          are provided here. */}
+          <Route path="/" element={<Layout />}>
+            {/* Child component Home*/}
+            <Route index element={<Home />} />
+            {/* Child component Protected*/}
+            <Route
+              path="/protected"
+              element={
+                <RequireAuth>
+                  <Protected />
+                </RequireAuth>
+              }
+            />
+            {/* Child component ProtectedSecond */}
+            <Route
+              path="/protected2"
+              element={
+                <RequireAuth>
+                  <ProtectedSecond pdfFile={pdfFile}/>
+                </RequireAuth>
+              }
+            />
+            <Route path="/create" element={
+            <RequireAuth>
+              <Create setPDFFile={setPDFFile} pdfFile={pdfFile}/>
+            </RequireAuth>
+            } />
+            {/* Child component Login */}
+            <Route path="/login" element={<Login />} />
+          {/* End of parent component Layout */}
+          </Route>
+  
+          
+        </Routes>
+      </BrowserRouter>
+    )
+  } /* End of Presentee Routes function */
+
+  export default PresenteeRoutes;
