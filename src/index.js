8a1ad08bb349a5bb2ad83d6ea15354cbfde64afe
clrminded <the_rocket_scientist@icloud.com>
2023-01-29 20:53:26 -0800

    reinstalled aws-amplify/ui-react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
-// import awsExports from './aws-exports';
-import { Amplify } from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import '@aws-amplify/ui-react/styles.css'
 import { AmplifyProvider } from "@aws-amplify/ui-react";
-import config from './aws-exports';
+import awsconfig from './aws-exports';
 
-// Amplify.configure(awsExports);
-Amplify.configure(config);
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
 ReactDOM.render(
+  <React.StrictMode>
     <AmplifyProvider>
         <App />
-    </AmplifyProvider>,
+    </AmplifyProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
