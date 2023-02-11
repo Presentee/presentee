f298445f6753f7c997cb3ac7ddbe88b3d6356e45
clrminded <the_rocket_scientist@icloud.com>
2023-02-11 13:40:30 -0800

    improved navigation on join page

--- src/routes/Join.jsx
+++ src/routes/Join.jsx
@@ -1,10 +1,12 @@
 // components/Protected.js
 import VContainer from 'components/CustomComponents/Containers';
 import Button from 'components/CustomComponents/Button';
+import Header from 'components/Header';
 
 const Join = () => {  
   return (
     <>
+      <Header/>
       <VContainer style={{display: "inline", marginTop: "30%"}}>
         <input type="text" className="custom-text-input" placeholder="Enter Presentee room ID" />
         <Button style={{width: 200, fontSize: "3rem"}}>Join</Button>
