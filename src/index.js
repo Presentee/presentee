cf67270913ff47a3be7506c2e874f9344ce7b747
clrminded <the_rocket_scientist@icloud.com>
2023-02-14 16:13:06 -0800

    added storage config to access s3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Storage } from 'aws-amplify';
 import ReactDOM from 'react-dom';
 
 import App from './App';
@@ -9,6 +9,7 @@ import '@aws-amplify/ui-react/styles.css';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
+Storage.configure(aws_exports);
 
 ReactDOM.render(
     <Authenticator.Provider>
