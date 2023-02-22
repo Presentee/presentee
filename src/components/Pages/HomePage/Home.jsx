c113f36443a03390f4cfdc200a49833b0ba05649
blackadder4 <55015249+blackadder4@users.noreply.github.com>
2023-02-22 01:13:16 -0800

    Made changes to the upload and list retrieve function
    New function will include the cognito user's name as prefix before upload to s3, the list function list only files uploaded by the user, and return a list of icons components with file names.

--- src/components/Pages/HomePage/Home.jsx
+++ src/components/Pages/HomePage/Home.jsx
@@ -11,6 +11,7 @@ import NavigationBar from 'components/Navigation';
 
 import Button from 'components/CustomComponents/Button'
 import { Storage } from "@aws-amplify/storage"
+import { Auth } from 'aws-amplify'; // import the Auth module from aws-amplify
 // ---------------- END OF IMPORTS -----------------------
 
 
@@ -19,11 +20,62 @@ async function retreiveFile() {
     level: 'public'
   });
 }
-
+//Leo's notes, this si the new list File function that search the user's name in the S3 storage,
+//This will return a Componenet with a list of files icons that would be used to retrieve file
+//will work on retrieve file later.
 async function listFiles() {
-  Storage.list('public/') // for listing ALL files without prefix, pass '' instead
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err));
+  try {
+    const user = await Auth.currentAuthenticatedUser();
+    const username = user.username;
+    const fileList = await Storage.list(`public/${username}-`);
+    const files = fileList.map((file) => file.key);
+
+    // Create an array of objects containing the file name and the corresponding icon
+    const fileIcons = files.map((file) => {
+      const extension = file.split('.').pop().toLowerCase();
+      const icon = getIconForExtension(extension);
+      return { file, icon };
+    });
+
+    // Render the list of files with icons
+    return (
+      <div className="file-list">
+        {fileIcons.map((item) => (
+          <div className="file-item">
+            <i className={`fa fa-${item.icon}`}></i>
+            <span>{item.file}</span>
+          </div>
+        ))}
+      </div>
+    );
+
+  } catch (error) {
+    console.log('Error listing files: ', error);
+  }
+}
+
+// Returns an icon for the given file extension
+function getIconForExtension(extension) {
+  switch (extension) {
+    case 'pdf':
+      return 'file-pdf-o';
+    case 'doc':
+    case 'docx':
+      return 'file-word-o';
+    case 'xls':
+    case 'xlsx':
+      return 'file-excel-o';
+    case 'ppt':
+    case 'pptx':
+      return 'file-powerpoint-o';
+    case 'jpg':
+    case 'jpeg':
+    case 'png':
+    case 'gif':
+      return 'file-image-o';
+    default:
+      return 'file-text-o';
+  }
 }
 
 
@@ -44,14 +96,22 @@ export default function Home() {
      it will upload the file into the s3 button, if upload is successful, 
      a message that it was successful, otherwise it will return a message 
      with an error */
-  const uploadFile = async (file) => {
-    try {
-      await Storage.put(file.name, file, { contentType: file.type });
-      setResponse(`Successfuly uploaded ${file.name}!`);
-    } catch (error) {
-      setResponse(`Error uploading file: ${error}`);
-    }
+  //Leo's edit, this new function appends the cognito's username infront of the uploaded file name
+  //I tried to get the file to include meta data about the owner but it didn't work so I guess this
+  //is the way to go for now, if I uploaded a File called Labreport.pdf
+  //it become Leo-Labreport.pdf
+const uploadFile = async (file) => {
+  try {
+    const user = await Auth.currentAuthenticatedUser(); // get the current authenticated user
+    const username = user.username; // get the username of the current authenticated user
+    const fileName = `${username}-${file.name}`;
+    await Storage.put(fileName, file, { contentType: file.type });
+    setResponse(`Successfully uploaded ${fileName}!`);
+  } catch (error) {
+    setResponse(`Error uploading file: ${error}`);
   }
+}
+
   
 
   return (
