c63aca5922eb04b404e9ca8976fee385c7636ef7
Christopher Liudahl-Rackley <the_rocket_scientist@icloud.com>
2023-05-31 16:51:08 -0700

    extracted out the url version from package.json

--- src/Pages/PresentingPage/Presenting.jsx
+++ src/Pages/PresentingPage/Presenting.jsx
@@ -6,16 +6,21 @@ import { scrollModePlugin } from '@react-pdf-viewer/scroll-mode'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
+const pdfjsVersion = require('pdfjs-dist/package.json').version;
+const workerUrl = `https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`;
+
 export default function Presentating(params) {
 
     const newplugin = defaultLayoutPlugin()
     const scrollModePluginInstance = scrollModePlugin();
     scrollModePluginInstance.switchScrollMode(ScrollMode.Page)
 
+
     return (
         <>
             <div className='pdf-container'>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+                {/*The workerURL now should update whenever the version updates */}
+                <Worker workerUrl={workerUrl}>
                     {params.pdfFile && <>
                         <Viewer fileUrl={params.pdfFile} plugins={[scrollModePluginInstance, newplugin]}/>
                     </>}
